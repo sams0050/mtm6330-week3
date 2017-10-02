557bf04dee733443ae4e616e8deb021da8c67289
sams0050 <sams0050@users.noreply.github.com>
2017-10-02 15:31:05 -0400

    Path fix

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,15 @@
 // Declare global scope variables.
 const gulp = require('gulp')      // load the gulp library
 const sass = require('gulp-sass') // load the gulp-sass compiler library
-
+const sassPath = 'scss/**/*.scss'
 // Define a new task called 'sass' that we can call to compile Sass to CSS
 gulp.task('sass', function () {
   return gulp
-    .src('scss/**/*.scss')  // where to find the Sass source files
+    .src(sassPath)  // where to find the Sass source files
     .pipe(sass())           // forward those files to the compiler
     .pipe(gulp.dest('css')) // where to output the comipled CSS files
 })
 
 gulp.task('default', function () {
-  gulp.watch('scss/**/*.scss', ['sass'])
+  gulp.watch(sassPath, ['sass'])
 })
